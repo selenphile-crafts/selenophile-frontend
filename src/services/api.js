f5252e5cb9a1262f7a9b314e174b24ca0ba968e9
selenphile-crafts <abbasbilal.info@gmail.com>
2026-05-20 15:14:57 +0530

    Updating the minor errors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+let baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+if (baseURL && !baseURL.endsWith('/api') && !baseURL.endsWith('/api/')) {
+  baseURL = baseURL.replace(/\/+$/, '') + '/api';
+}
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL,
   headers: { 'Content-Type': 'application/json' }
 });
 
